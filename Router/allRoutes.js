d7efa50976d6e2032df098b1370b59c4bc052149
naszegrer <zegrer1964@hotmail.com>
2023-12-31 21:46:21 +0100

    Gestion du compte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,12 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),];
-
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html" ),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword","Changement de mot de passe", "/pages/auth/editPassword.html"),
+];
+   
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
